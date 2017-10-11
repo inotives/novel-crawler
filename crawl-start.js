d0396cf02544ac7d0e12e421b72bacdaa3234aa3
Toni Lim <tonilim85@gmail.com>
2017-10-11 17:36:27 +0800

    adding dynamic link to dwonload listing

--- crawl-start.js
+++ crawl-start.js
@@ -2,10 +2,11 @@ var Crawler = require("crawler");
 var htmlToText = require('html-to-text');
 var fs = require('fs');
 
-var saveFolder = 'wmw'
+
 var urlBase = process.argv[2]
 var chapRange = process.argv[3].split('-')
 var downloadList = []
+var saveFolder = 'data/'+urlBase.split('/')[3].split('-')[0]+'/'
 
 var c = new Crawler({
     maxConnections : 10,
@@ -26,17 +27,14 @@ var c = new Crawler({
             // console.log(text)
             var cleanedText = raw.split('----------------------------------------------------------------------------------------------------------------------------------')[1]
 
-            fs.writeFile('data/'+saveFolder+"/"+chapName+".txt", cleanedText , function(err){
+            fs.writeFile(saveFolder+chapName+".txt", cleanedText , function(err){
               if(err) { return console.log(err); }
-              console.log("The file was saved!");
+              console.log("Chapter:: "+chapName+" was saved!!");
             })
         }
         done();
     }
 });
-
-
-
 
 for(i=chapRange[0]; i <= chapRange[1]; i++){
   var url = urlBase + '-' + i
@@ -46,3 +44,5 @@ for(i=chapRange[0]; i <= chapRange[1]; i++){
 console.log(downloadList)
 
 c.queue(downloadList);
+
+//command:: node crawl-start.js http://www.wuxiaworld.com/wmw-index/wmw-chapter 1-100
